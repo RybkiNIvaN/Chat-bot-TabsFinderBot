6128120635b6837786b11ad8c98db75779271cb1
4117 Ivan Rybkin <ivan9643721960@gmail.com>
2023-05-07 10:38:57 +0300

    some changes

---
+++ removing_letters.py
@@ -0,0 +1,22 @@
+def remove_letters(word):
+    if 'ь' in word: word = word.replace('ь', ' ')
+
+    if 'ая' in word:
+        word = word.replace('ая', ' ')
+    elif 'я' in word:
+        word = word.replace('я', ' ')
+
+    if 'ж' in word: word = word.replace('ж', ' ')
+
+    if 'ч' in word: word = word.replace('ч', ' ')
+
+    if 'ш' in word: word = word.replace('ш', ' ')
+
+    if 'ый' in word:word = word.replace('ый', ' ')
+    elif 'й' in word: word = word.replace('й', ' ')
+    elif 'ы' in word: word = word.replace('ы', ' ')
+
+    if 'ъ' in word:
+        word = word.replace('ъ', ' ')
+
+    return word
--- transliter.py
+++ transliter.py
@@ -1,22 +1,24 @@
 import os
 import ords
-import main
 import transliterate
+import removing_letters
 from transliterate import translit
-lang = 'ru'
-your_group = 'Король и шут'
+
 def first_letter_number(your_group):
-    return ords.first_letters_dict[your_group[0]]
+    return ords.first_letters_dict[your_group[0].lower()]
 
 def go_to_firstletter_path(my_path, letter_number, lang):
-    if lang == 'en':
+    if lang == 'en' or 'ан':
         next_path = letter_number
         os.chdir(os.path.join(my_path, str(next_path)))
-    elif lang == 'ru':
+    elif lang == 'ru' or 'ру':
         next_path = letter_number
         os.chdir(os.path.join(my_path, str(next_path)))
 
 def group_finder(my_path, your_group):
+    your_group = removing_letters.remove_letters(your_group)
+    your_group = translit(your_group, language_code='ru', reversed=True)
+    print(your_group)
     group = your_group.split()
     list_of_sets = []
     def set_generator(group):
@@ -26,27 +28,46 @@ def group_finder(my_path, your_group):
         return my_set
     for k in group:
         list_of_sets.append(set_generator(k))
-    #print(list_of_sets)
 
+    print(list_of_sets)
     final_set = set(list_of_sets[0])
 
-    for i in range(1,len(list_of_sets)):
+    for i in range(1, len(list_of_sets)):
         final_set = final_set.intersection(list_of_sets[i])
-    #if len(list_of_sets) == 1: return final_set
-    #else:
-        #final_set = final_set.intersection(list_of_sets[1])
-        #for k in range(len(list_of_sets)):
-            #final_set =
+
+    final_list = list(final_set)
+    print(str(final_list[0]))
+    os.chdir(os.path.join(my_path, str(final_list[0])))
+
     return final_set
 
+def song_finder(my_path, your_song):
+    your_song = removing_letters.remove_letters(your_song)
+    your_song = translit(your_song, language_code='ru', reversed=True)
+    print(your_song)
+    song = your_song.split()
+    list_of_sets = []
+
+    def set_generator(song):
+        my_set = set()
+        for j in os.listdir(my_path):
+            if song in j: my_set.add(j)
+        return my_set
+
+    for k in song:
+        list_of_sets.append(set_generator(k))
+
+    print(list_of_sets)
+    final_set = set(list_of_sets[0])
+
+    for i in range(1, len(list_of_sets)):
+        final_set = final_set.intersection(list_of_sets[i])
+
+    final_list = list(final_set)
+    print(str(final_list[0]))
+    return str(final_list[0])
 
 
-os.chdir(r"C:\Users\steve\PycharmProjects\pythonProject1\For_Project\Tabs")
-print(os.getcwd())
-my_path = os.getcwd()
-go_to_firstletter_path(my_path, first_letter_number(your_group), lang)
-my_path = os.getcwd()
-your_group = translit(your_group, language_code='ru', reversed=True)
-print(group_finder(my_path, your_group))
-#print(os.listdir(my_path))
-#print(os.getcwd())
+
+
+
