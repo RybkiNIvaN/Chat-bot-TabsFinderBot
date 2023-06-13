d083864339b2e53d881595c94592185d4cad3ed8
RybkiNIvaN <113449250+RybkiNIvaN@users.noreply.github.com>
2023-06-13 16:51:12 +0300

    Update transliter.py

--- BOT/transliter.py
+++ BOT/transliter.py
@@ -16,7 +16,10 @@ def go_to_firstletter_path(my_path, letter_number):
 
 def all_finder(my_path, what_to_find, group_or_song):
     what_to_find = removing_letters.remove_letters(what_to_find)
-    what_to_find = translit(what_to_find, language_code='ru', reversed=True)
+    what_to_find = translit(what_to_find,
+                            language_code='ru',
+                            reversed=True
+                            )
     print('4.1', what_to_find)
     to_find = what_to_find.split()
     list_of_sets = []
@@ -34,14 +37,16 @@ def all_finder(my_path, what_to_find, group_or_song):
     if list_of_sets == [set()]:
         return list_of_sets
     final_set = set(list_of_sets[0])
-    for i in range(1, len(list_of_sets)-1):
+    for i in range(0, len(list_of_sets)-1):
         final_set = final_set.intersection(list_of_sets[i])
         print(final_set)
+    if final_set == set():
+        return set()
     final = list(final_set)
-    print('4.3', final[0])
     if group_or_song == 'group':
+        print('4.3', final[0])
         os.chdir(os.path.join(my_path, str(final[0])))
         return str(final[0])
     elif group_or_song == 'song':
-        return str(final[0])
+        return list_of_sets[0]
 
